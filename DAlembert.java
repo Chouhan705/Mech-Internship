c073c056fd011b6d08e6aa24061f2b2e2573b68b
Raina Girish <157197240+raina-12@users.noreply.github.com>
2024-07-13 18:17:21 +0530

    Update DAlembert.java

--- DAlembert.java
+++ DAlembert.java
@@ -10,43 +10,51 @@ public class DAlembert
         switch(choice)
         {
             case 1:
-            System.out.println("Choose what you want find:");
-            System.out.println("1. Force");
-            System.out.println("2. Acceleration");
-            int quantity = sc.nextInt();
-            if(quantity==1)
-            {
-                force();
-                break;
-            }
-
             System.out.println("Choose whether it is an/a..");
             System.out.println("1. One Block system");
-            System.out.println("2. Multiple Block system");
+            System.out.println("2. Multiple Block system"); 
             int system = sc.nextInt();
+
+            System.out.println("Choose what you want find:");
+            System.out.println("1. Acceleration & some other quantity");
+            System.out.println("2. Acceleration and Tension");
+            int quantity = sc.nextInt();
       
             if(system==1)
             {
-                oneBlock();
+                if(quantity==1)
+                {
+                    oneBlock();
+                }
+            }
+            if(system==2)
+            {
+                if(quantity==1)
+                {
+                    multipleBlock();
+                }
             }
             if (system==2)
             {
-                multipleBlock();
+                if(quantity==2)
+                {
+                    TwoBlocksWithRopeOnTheSamePlane();
+                }
             }
             break;
 
-           case 2:
-            System.out.println("Choose the operation to perform");
-            System.out.println("1. Find tension with One Block");
-            System.out.println("2. Find tension with Two blocks");
+            case 2:
+            System.out.println("Choose the operation to perform on");
+            System.out.println("1. One block pulley system");
+            System.out.println("2. Two block pulley system");
             int rope = sc.nextInt();
             if(rope==1)
             {
-                Pulleysystem();
+                oneBlockPulleysystem();
             }
             else if(rope == 2)
             {
-                TwoBlocksWithRope();
+                twoBlockPulleysystem();
             }
             else
             {
@@ -58,6 +66,7 @@ public class DAlembert
             System.out.println("Invalid choice");
             break;
         }
+        sc.close();
     }
 
     public static void Formula()
@@ -68,7 +77,7 @@ public class DAlembert
         switch(formula)
         {
             case 1:
-            System.out.println("Enter the value of the following quantities in this order....[intial velocity, acceleration & time]");
+            System.out.println("Enter the value of the following quantities in this order...[intial velocity, acceleration & time]");
              u = sc.nextDouble();
              a = sc.nextDouble();
              t = sc.nextDouble();
@@ -77,7 +86,7 @@ public class DAlembert
             break;
 
             case 2:
-            System.out.println("Enter the value of the following quantities in this order....[intial velocity, acceleration & distance]");
+            System.out.println("Enter the value of the following quantities in this order...[intial velocity, acceleration & distance]");
             u = sc.nextDouble();
             a = sc.nextDouble();
             s = sc.nextDouble();
@@ -86,7 +95,7 @@ public class DAlembert
             break;
 
             case 3:
-            System.out.println("Enter the value of the following quantities in this order....[intial velocity, acceleration & time]");
+            System.out.println("Enter the value of the following quantities in this order...[intial velocity, acceleration & time]");
             u = sc.nextDouble();
             a = sc.nextDouble();
             t = sc.nextDouble();
@@ -95,7 +104,7 @@ public class DAlembert
             break;
 
             case 4:
-            System.out.println("Enter the value of the following quantities in this order....[final velocity, initial velocity & acceleration]");
+            System.out.println("Enter the value of the following quantities in this order...[final velocity, initial velocity & acceleration]");
             v = sc.nextDouble();
             u = sc.nextDouble();
             a = sc.nextDouble();
@@ -104,7 +113,7 @@ public class DAlembert
             break;
 
             case 5:
-            System.out.println("Enter the value of the following quantities in this order....[final velocity, initial velocity & acceleration]");
+            System.out.println("Enter the value of the following quantities in this order...[final velocity, initial velocity & acceleration]");
             v = sc.nextDouble();
             u = sc.nextDouble();
             a = sc.nextDouble();
@@ -113,7 +122,7 @@ public class DAlembert
             break;
 
             case 6:
-            System.out.println("Enter the value of the following quantities in this order....[final velocity, initial velocity & distance]");
+            System.out.println("Enter the value of the following quantities in this order...[final velocity, initial velocity & distance]");
             v = sc.nextDouble();
             u = sc.nextDouble();
             s = sc.nextDouble();
@@ -122,7 +131,7 @@ public class DAlembert
             break;
 
             case 7:
-            System.out.println("Enter the value of the following quantities in this order....[final velocity, initial velocity & time]");
+            System.out.println("Enter the value of the following quantities in this order...[final velocity, initial velocity & time]");
             v = sc.nextDouble();
             u = sc.nextDouble();
             t = sc.nextDouble();
@@ -130,22 +139,8 @@ public class DAlembert
             System.out.println("The acceleration is "+(a)+ " m/s^2");
             break;
         }
+        sc.close();
     }
-        public static void force()
-        {
-            double g = 9.81;
-            Scanner sc  = new Scanner(System.in);
-            System.out.println("Enter the mass of the block (if weight is given kindly divide it by 9.81 to obtain the mass:)");
-            double mass = sc.nextDouble();
-            System.out.println("Enter the value of frictional constant:");
-            double mu = sc.nextDouble();
-            System.out.println("Enter the angle(in degrees) of force from positive x-axis:");
-            double theta = Math.toRadians(sc.nextDouble());
-            System.out.println("Enter the acceleration:");
-            double acceleration = sc.nextDouble();
-            double F = ((mass*acceleration) + (mu*mass*g))/(Math.cos(theta) + mu*Math.sin(theta));                  // giving the right answer when in the denominator (sign = + & theta =330) & (sign = - & theta =30)
-            System.out.println("The force acting on the block is "+(F));
-        }
 
         public static void oneBlock()
         {
@@ -157,7 +152,7 @@ public class DAlembert
             System.out.println("Enter the angle of inclination (in degrees):");
             double theta = Math.toRadians(sc.nextDouble());
             double acceleration = 9.81*(Math.sin(theta)- mu*Math.cos(theta));
-            System.out.println("The acceleration of the block is "+(acceleration));
+            System.out.println("The acceleration of the block is "+(acceleration)+ " m/s^2");
 
             System.out.println("\n Choose what else you want to find and the formula to find it\n 1. v=u+at\n 2. v^2 = u^2 + 2as\n 3. s = ut + 1/2at^2\n 4. s = (v^2-u^2)/2a\n 5. t = (v-u)/a\n 6. a = (v^2-u^2)/2s\n 7. a = (v-u)/t");
             Formula();
@@ -172,41 +167,49 @@ public class DAlembert
             {
                 System.out.println("okay :)");
             }
+            sc.close();
         }
 
         public static void multipleBlock()
         {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the number of blocks:");
-            int n = sc.nextInt();  
+            int n = sc.nextInt();
+            System.out.println("First enter the details of the block which is placed lower on the inclined plane");
+            double [] Acceleration = new double[n+1]; 
             for(int i=1; i<=n; i++)
             {
-                System.out.println("Enter the mass of the block (if weight is given kindly divide it by 9.81 to obtain the mass:)");
+                System.out.println("Enter the mass of the block (if weight is given kindly divide it by 9.81 to obtain the mass):");
                 double mass = sc.nextDouble();
                 System.out.println("Enter the value of frictional constant:");
                 double mu = sc.nextDouble();
                 System.out.println("Enter the angle of inclination (in degrees):");
                 double theta = Math.toRadians(sc.nextDouble());
                 double acceleration = 9.81*(Math.sin(theta)- mu*Math.cos(theta));
-                System.out.println("The acceleration of the block is "+(acceleration));
+                System.out.println("The acceleration of the block is "+(acceleration)+ " m/s^2");
+                Acceleration[i]= acceleration;  
             }
-            System.out.println("\n Choose what else you want to find and the formula to find it\n 1. v=u+at\n 2. v^2 = u^2 + 2as\n 3. s = ut + 1/2at^2\n 4. s = (v^2-u^2)/2a\n 5. t = (v-u)/a\n 6. a = (v^2-u^2)/2s\n 7. a = (v-u)/t");
-            Formula();
-            System.out.println("Do you wish to find some other physical quantitty too?\n If yes enter 1 else enter 0");
-            double wish = sc.nextDouble();
-            if (wish==1)
-            {
-                System.out.println("Choose what else you want to find and the formula to find it\n 1. v=u+at\n 2. v^2 = u^2 + 2as\n 3. s = ut + 1/2at^2\n 4. s = (v^2-u^2)/2a\n 5. t = (v-u)/a\n 6. a = (v^2-u^2)/2s\n 7. a = (v-u)/t");
-                Formula();
-            }
-            if (wish==0)
-            {
-                System.out.println("okay :)");
-            }
-        }
+            System.out.println("Enter the distance between the blocks:");
+            double x = sc.nextDouble();
+            //d = 0.5*a*t*t    (for block A)
+            //d+x = 0.5*a*t*t    (for block B)   
+            //eqn 1 : 1(d) - 0.5*a*(t*t) = 0
+            // eqn 2 : -1(d) + 0.5*a*(t*t) = x
+            double CoefX1 = 1;
+            double CoefY1 = -0.5*Acceleration[1];
+            double CoefX2 = -1;
+            double CoefY2 = 0.5*Acceleration[2];
+            double Const1 = 0;
+            double Const2 = x;
+            double[] Answer = SimultaneusEqnSolver(CoefX1,CoefY1,Const1,CoefX2,CoefY2,Const2);
+            double distance = Answer[0];
+            double time = Answer[1];
+            System.out.println("The total distance travelled by the blocks "+(distance)+ " m");
+            System.out.println("The time when the blocks collide "+(Math.sqrt(time))+ " s");
+            sc.close();
+          }
 
-
-    public static void Pulleysystem() 
+    public static void oneBlockPulleysystem() 
     {
         double g = 9.81;
         Scanner sc = new Scanner(System.in);
@@ -218,73 +221,72 @@ public class DAlembert
         double acceleration = sc.nextDouble();
         double Tension = (mass*g)-(mass*acceleration);
         System.out.println("The tension in the string is "+(Tension)+ " N");
+        sc.close();
     }
-    public static void TwoBlocksWithRope()
+
+    public static void twoBlockPulleysystem()
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the mass of the block force is applied on( gravitational too ) [divide by 9.81 if weight is given]");
+        System.out.println("Enter the mass of the block force is applied on (gravitational too) [divide by 9.81 if weight is given]");
         double mass1 = sc.nextDouble();
-        System.out.println("Enter the mass of the second block[divide by 9.81 if weight is given]");
+        System.out.println("Enter the mass of the second block [divide by 9.81 if weight is given]");
         double mass2 = sc.nextDouble();
         System.out.println("Enter the frictional constant (if none enter 1)");
         double mu = sc.nextDouble();
-        System.out.println("Choose the arrangement of the blocks");
-        System.out.println("1. On the same plane");
-        System.out.println("2. Hanging on the sides of a pulley");
-        int arrangement = sc.nextInt();
-        switch(arrangement)
-        {
-            case 1:
-            System.out.println("Enter the force applied");
-            double force = sc.nextDouble();
-            double normal1 = (mass1*9.81);
-            double normal2 = (mass2*9.81);
-            // eq1 force - t - mu*N1 = m1(a)
-            // eq2 t-mu*N2 = m2(a)
-
-            // m1(a) + t = F - mu*N1
-            // m2(a) - t = - mu*N2
-            double Coefx1=mass1;
-            double Coefy1 = 1;
-            double Const1 = force - mu*normal1;
-            double Coefx2 = mass2;
-            double Coefy2 = -1;
-            double Const2 = - mu*normal2;
-            double[] Answer = SimultaneusEqnSolver(Coefx1,Coefy1,Const1,Coefx2,Coefy2,Const2);
-            double Acceleration = Answer[0];
-            double Tension = Answer[1];
-            System.out.printf("The acceleration of the blocks is %.2f ",(Acceleration));
-            System.out.printf("m/s^2");
-            System.out.println();
-            System.out.printf("The tension in the string is %.2f",(Tension));
-            System.out.printf("N");
-            break;
-
-            case 2:
-            double normal = mass2*9.81;
-            // m1(a) + t = m1*g
-            // m2(a) - t = - mu*N
-            Coefx1=mass1;
-            Coefy1 = 1;
-            Const1 = mass1*9.81;
-            Coefx2 = mass2;
-            Coefy2 = -1;
-            Const2 = - mu*normal;
-            Answer = SimultaneusEqnSolver(Coefx1,Coefy1,Const1,Coefx2,Coefy2,Const2);
-            Acceleration = Answer[0];
-            Tension = Answer[1];
-            System.out.printf("The acceleration of the blocks is %.2f ",(Acceleration));
-            System.out.printf("m/s^2");
-            System.out.println();
-            System.out.printf("The tension in the string is %.2f",(Tension));
-            System.out.printf("N");
-            break;
-
-            default:
-            System.out.println("Invalid choice");
-            break;
-        }
+        double normal = mass2*9.81;
+        // m1(a) + t = m1*g
+        // m2(a) - t = - mu*N
+        double Coefx1=mass1;
+        double Coefy1 = 1;
+        double Const1 = mass1*9.81;
+        double Coefx2 = mass2;
+        double  Coefy2 = -1;
+        double Const2 = - mu*normal;
+        double[] Answer = SimultaneusEqnSolver(Coefx1,Coefy1,Const1,Coefx2,Coefy2,Const2);
+        double Acceleration = Answer[0];
+        double Tension = Answer[1];
+        System.out.printf("The acceleration of the blocks is %.2f ",(Acceleration));
+        System.out.printf("m/s^2");
+        System.out.println();
+        System.out.printf("The tension in the string is %.2f",(Tension));
+        System.out.printf("N");
+        sc.close();
     }
+
+    public static void TwoBlocksWithRopeOnTheSamePlane()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the mass of the block force is applied on (gravitational too) [divide by 9.81 if weight is given]");
+        double mass1 = sc.nextDouble();
+        System.out.println("Enter the mass of the second block [divide by 9.81 if weight is given]");
+        double mass2 = sc.nextDouble();
+        System.out.println("Enter the frictional constant (if none enter 1)");
+        double mu = sc.nextDouble();
+        System.out.println("Enter the force applied");
+        double force = sc.nextDouble();
+        double normal1 = (mass1*9.81);
+        double normal2 = (mass2*9.81);
+        // eq1 force - t - mu*N1 = m1(a)
+        // eq2 t-mu*N2 = m2(a)
+
+        // m1(a) + t = F - mu*N1
+        // m2(a) - t = - mu*N2
+        double Coefx1=mass1;
+        double Coefy1 = 1;
+        double Const1 = force - mu*normal1;
+        double Coefx2 = mass2;
+        double Coefy2 = -1;
+        double Const2 = - mu*normal2;
+        double[] Answer = SimultaneusEqnSolver(Coefx1,Coefy1,Const1,Coefx2,Coefy2,Const2);
+        double Acceleration = Answer[0];
+        double Tension = Answer[1];
+        System.out.printf("The acceleration of the blocks is %.2f ",(Acceleration));
+        System.out.printf("m/s^2");
+        System.out.println();
+        System.out.printf("The tension in the string is %.2f",(Tension));
+        System.out.printf("N");      
+    }
+
     public static double[] SimultaneusEqnSolver(double CoefX1 , double CoefY1 , double Const1 , double CoefX2 , double CoefY2 , double Const2)
     {
         //Coefx1(x) + Coefy1(y) = Const1
